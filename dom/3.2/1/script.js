138805ce4d6c8eb4b6159e3069975162887350f8
Vladimir Belich <belichvladimir@mail.ru>
2023-06-28 12:58:20 +0300

    fix 3.2 1

--- dom/3.2/1/script.js
+++ dom/3.2/1/script.js
@@ -1,22 +1,7 @@
-const btn1 = document.querySelector('.btn1');
-const btn2 = document.querySelector('.btn2');
-const btn3 = document.querySelector('.btn3');
-const btn4 = document.querySelector('.btn4');
+const btns = document.querySelectorAll('.btn');
 
-if (btn1) {
-btn1.addEventListener('click',()=> {
-    location.assign('./1.html');
-})}
-if (btn2) {
-btn2.addEventListener('click',()=> {
-    location.assign('./2.html');
-})}
-if (btn3) {
-btn3.addEventListener('click',()=> {
-    location.assign('./3.html');
-})}
-if (btn4) {
-btn4.addEventListener('click',()=> {
-    location.assign('./index.html');
-})}
-
+btns.forEach((btn) => {
+  btn.addEventListener('click', (event) => {
+    location.assign(event.target.dataset.href);
+  });
+});
