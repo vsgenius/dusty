0b1990d0e12bc715d02ce005ff01e2c552fcaae5
Vladimir Belich <belichvladimir@mail.ru>
2023-06-28 13:49:09 +0300

    fix 3.2 2

--- dom/3.2/2/script.js
+++ dom/3.2/2/script.js
@@ -1,29 +1,37 @@
 const btn = document.querySelector('.btn');
+const inputAlert = document.querySelector('.warning');
+
 btn.addEventListener('click', (e) => {
-  const input = document.querySelector('#text');
-  if (!input.value) return;
-  else if (input.value === 'назад') {
-    input.value = '';
+  let value = document.querySelector('#text').value;
+
+  if (!value) return;
+  if (value === 'назад') {
+    value = '';
     history.back();
     return;
-  } else if (input.value === 'вперед') {
-    input.value = '';
+  }
+  if (value === 'вперед') {
+    value = '';
     history.forward();
     return;
-  } else if (input.value.split(' ')[0] === 'скопируй') {
-    navigator.clipboard.writeText(input.value.split(' ')[1]);
-    input.value = '';
+  }
+  if (value.startsWith('скопируй')) {
+    navigator.clipboard.writeText(value.slice(9));
+    value = '';
     return;
-  } else if (input.value === 'обнови') {
-    input.value = '';
+  }
+  if (value === 'обнови') {
+    value = '';
     location.reload();
     return;
-  } else if (
-    input.value.split(' ')[0] === 'переадресуй' &&
-    input.value.split(' ')[1] === 'на'
-  ) {
-    location.assign(input.value.split(' ')[2]);
-    input.value = '';
+  }
+  if (value.startsWith('переадресуй на')) {
+    location.assign(value.slice(14));
+    value = '';
     return;
-  } else alert('такого не умею');
+  }
+  inputAlert.classList.remove('hidden');
+  setTimeout(() => {
+    inputAlert.classList.add('hidden');
+  }, 1000);
 });
