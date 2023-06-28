8cbc8e322f9e7359a4933acc7b876fea1613256d
Vladimir Belich <belichvladimir@mail.ru>
2023-06-28 17:46:55 +0300

    fix 3.1 3

--- dom/3.1/3/script.js
+++ dom/3.1/3/script.js
@@ -1,15 +1,19 @@
 const triangle = document.querySelector('.triangle');
 
-triangle.addEventListener('click',(e) => {
-     if (e.target.style.transform === 'rotate(90deg)') {
-        e.target.style.transform = 'rotate(1800deg)';
-        const element = e.target.parentElement.parentElement.querySelector('.element') ;
-        e.target.parentElement.parentElement.removeChild(element);
-    } else {
-        e.target.style.transform = 'rotate(90deg)';
-        const div = document.createElement('div');
-        div.classList.add('element')
-        div.textContent = 'Хинка́ли (груз. ხინკალი) — блюдо грузинской кухни из горных областей Пшави, Мтиулети и Хевсурети в Грузии, получившее распространение в других районах Кавказа и по всему бывшему СССР. Разновидность дамплингов.'
-        e.target.parentElement.parentElement.appendChild(div);
-    }
-})
+triangle.addEventListener('click', (event) => {
+  const target = event.target;
+  console.log(target.classList.contains('rotate'));
+  if (target.classList.contains('rotate')) {
+    target.classList.remove('rotate');
+    const element =
+      target.parentElement.parentElement.querySelector('.element');
+    target.parentElement.parentElement.removeChild(element);
+  } else {
+    target.classList.add('rotate');
+    const div = document.createElement('div');
+    div.classList.add('element');
+    div.textContent =
+      'Хинка́ли (груз. ხინკალი) — блюдо грузинской кухни из горных областей Пшави, Мтиулети и Хевсурети в Грузии, получившее распространение в других районах Кавказа и по всему бывшему СССР. Разновидность дамплингов.';
+    target.parentElement.parentElement.appendChild(div);
+  }
+});
