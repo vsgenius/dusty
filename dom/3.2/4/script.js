df5e20f5b89ea66b5bbdab5ca54faf5984153ee8
Vladimir Belich <belichvladimir@mail.ru>
2023-06-28 14:15:38 +0300

    fix 3.2 4

--- dom/3.2/4/script.js
+++ dom/3.2/4/script.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector('button');
 const body = document.querySelector('body');
+
 document.addEventListener('DOMContentLoaded', () => {
   if (!localStorage.getItem('blacktheme')) {
     btn.classList.remove('btn-black');
@@ -11,15 +12,14 @@ document.addEventListener('DOMContentLoaded', () => {
     localStorage.setItem('blacktheme', true);
   }
 });
-btn.addEventListener('click', (e) => {
+btn.addEventListener('click', (event) => {
   if (localStorage.getItem('blacktheme')) {
-    e.target.classList.remove('btn-black');
+    event.target.classList.remove('btn-black');
     body.classList.remove('body-black');
     localStorage.removeItem('blacktheme');
   } else {
-    e.target.classList.add('btn-black');
+    event.target.classList.add('btn-black');
     body.classList.add('body-black');
     localStorage.setItem('blacktheme', true);
   }
-  console.log(localStorage.getItem('blacktheme'));
 });
