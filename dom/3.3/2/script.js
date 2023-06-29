7ab5364bb324f2909e31e9d60b73b2af8f3d5ebd
Vladimir Belich <belichvladimir@mail.ru>
2023-06-29 13:47:55 +0300

    fix 3.3 2

--- dom/3.3/2/script.js
+++ dom/3.3/2/script.js
@@ -1,8 +1,6 @@
-const blocks = document.querySelectorAll('.block');
+const blockDigits = document.querySelector('.block-digits');
 const text = document.querySelector('#text');
-let flag = false;
-let i = 0;
-let countChar = 0;
+
 function replaceChar(origString, replaceChar, index) {
   let firstPart = origString.substr(0, index);
   let lastPart = origString.substr(index + 1);
@@ -10,28 +8,27 @@ function replaceChar(origString, replaceChar, index) {
   let newString = firstPart + replaceChar + lastPart;
   return newString;
 }
+let i = 0;
+let countChar = 0;
 let lastTarget = undefined;
 var lastClick = 0;
-blocks.forEach((block) => {
-  block.addEventListener('click', (e) => {
-    e.preventDefault;
-    var d = new Date();
-    var t = d.getTime();
-    if (
-      (lastTarget !== undefined && t - lastClick > 1000) ||
-      (lastTarget !== undefined && lastTarget !== e.target.parentElement)
-    ) {
-      ++countChar;
-    }
-    const value = text.value;
-    const lishChar =
-      e.target.parentElement.querySelector('.chars').textContent +
-      e.target.parentElement.querySelector('.digit').textContent;
-    console.log(lishChar[i]);
-    text.value = replaceChar(value, lishChar[i], countChar);
-    lastClick = t;
-    ++i;
-    lastTarget = e.target.parentElement;
-    if (lishChar.length == i) i = 0;
-  });
+
+blockDigits.addEventListener('click', (event) => {
+  const target = event.target;
+  var date = new Date();
+  var time = date.getTime();
+  if (
+    (lastTarget !== undefined && time - lastClick > 1000) ||
+    (lastTarget !== undefined && lastTarget !== target.parentElement)
+  ) {
+    ++countChar;
+    i = 0;
+  }
+  const value = text.value;
+  const lishChar = target.parentElement.dataset.text;
+  text.value = replaceChar(value, lishChar[i], countChar);
+  lastClick = time;
+  ++i;
+  lastTarget = target.parentElement;
+  if (lishChar.length == i) i = 0;
 });
