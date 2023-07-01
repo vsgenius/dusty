ee87eae3c5589db1e155b6b23673d17b94920f4f
Vladimir Belich <belichvladimir@mail.ru>
2023-07-01 12:12:11 +0300

    fix 3.1 2 3

--- dom/3.1/2/script.js
+++ dom/3.1/2/script.js
@@ -2,12 +2,10 @@ const bug = document.querySelector('.bug');
 
 const btnsGroup = document.querySelector('.btn-group');
 btnsGroup.addEventListener('click', (event) => {
-    const target = event.target;
-    let {top, bottom,right,left} = bug.getBoundingClientRect();
-    bug.style.top = top + Number(target.dataset.top) + 'px';
-    bug.style.bottom = bottom + Number(target.dataset.bottom) + 'px';
-    bug.style.right = right + Number(target.dataset.right) + 'px';
-    bug.style.left = left + Number(target.dataset.left) + 'px';
-    // bug.style[e.target.classList[0]] = rect[e.target.classList[0]] + 10 + 'px';
-    // bug.style[e.target.classList[1]] = rect[e.target.classList[1]] - 10 + 'px';
-  });
+  const target = event.target;
+  const { top, bottom, right, left } = bug.getBoundingClientRect();
+  bug.style.top = top + Number(target.dataset.top) + 'px';
+  bug.style.bottom = bottom + Number(target.dataset.bottom) + 'px';
+  bug.style.right = right + Number(target.dataset.right) + 'px';
+  bug.style.left = left + Number(target.dataset.left) + 'px';
+});
--- dom/3.1/3/script.js
+++ dom/3.1/3/script.js
@@ -2,7 +2,6 @@ const triangle = document.querySelector('.triangle');
 
 triangle.addEventListener('click', (event) => {
   const target = event.target;
-  console.log(target.classList.contains('rotate'));
   if (target.classList.contains('rotate')) {
     target.classList.remove('rotate');
     const element =
@@ -14,6 +13,6 @@ triangle.addEventListener('click', (event) => {
     div.classList.add('element');
     div.textContent =
       'Хинка́ли (груз. ხინკალი) — блюдо грузинской кухни из горных областей Пшави, Мтиулети и Хевсурети в Грузии, получившее распространение в других районах Кавказа и по всему бывшему СССР. Разновидность дамплингов.';
-    target.parentElement.parentElement.appendChild(div);
+    target.closest('.container').appendChild(div);
   }
 });
