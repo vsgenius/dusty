35c3fd0aca29c806db0800860dc005ff046cc74d
Vladimir Belich <belichvladimir@mail.ru>
2023-07-03 13:41:42 +0300

    fix 3.3 1

--- dom/3.3/1/script.js
+++ dom/3.3/1/script.js
@@ -14,25 +14,23 @@ function weak(text) {
   return text.match(/^.*(?=.{5,})(?=.*[a-z]).*$/);
 }
 
-text.addEventListener('keyup', (e) => {
-  console.log(text.value);
+text.addEventListener('input', (e) => {
   checkPassword.textContent = '';
+  if (!text.value) {
+    return;
+  }
   if (strong(text.value)) {
     checkPassword.textContent = 'Отличный';
     return;
-  }
-  if (good(text.value)) {
+  } else if (good(text.value)) {
     checkPassword.textContent = 'Хороший';
     return;
-  }
-  if (okay(text.value)) {
+  } else if (okay(text.value)) {
     checkPassword.textContent = 'Нормальный';
     return;
-  }
-  if (weak(text.value)) {
+  } else if (weak(text.value)) {
     checkPassword.textContent = 'Слабый';
     return;
-  }
-  if (text.value.length < 5 && text.value.length > 0)
-    checkPassword.textContent = 'Короткий';
+  } else text.value.length < 5 && text.value.length > 0;
+  checkPassword.textContent = 'Короткий';
 });
