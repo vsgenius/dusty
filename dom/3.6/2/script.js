8f639753bff95523eeb0386ac900064ae557089c
Vladimir Belich <belichvladimir@mail.ru>
2023-07-04 21:04:32 +0300

    fix 3.6 2

--- dom/3.6/2/script.js
+++ dom/3.6/2/script.js
@@ -1,13 +1,25 @@
 const bug = document.querySelector('.bug');
 
+function bugRun(key) {
+  const { top, bottom, right, left } = bug.getBoundingClientRect();
+  if (key === 'ArrowUp' || key === 'up') {
+    bug.style.top = top - 10 + 'px';
+    bug.style.bottom = bottom + 10 + 'px';
+  } else if (key === 'ArrowDown' || key === 'down') {
+    bug.style.top = top + 10 + 'px';
+    bug.style.bottom = bottom - 10 + 'px';
+  } else if (key === 'ArrowLeft' || key === 'left') {
+    bug.style.right = right + 10 + 'px';
+    bug.style.left = left - 10 + 'px';
+  } else {
+    bug.style.right = right - 10 + 'px';
+    bug.style.left = left + 10 + 'px';
+  }
+}
 const btnsGroup = document.querySelector('.btn-group');
 btnsGroup.addEventListener('click', (event) => {
   const target = event.target;
-  const { top, bottom, right, left } = bug.getBoundingClientRect();
-  bug.style.top = top + Number(target.dataset.top) + 'px';
-  bug.style.bottom = bottom + Number(target.dataset.bottom) + 'px';
-  bug.style.right = right + Number(target.dataset.right) + 'px';
-  bug.style.left = left + Number(target.dataset.left) + 'px';
+  bugRun(target.dataset.run);
 });
 
 document.addEventListener('keydown', (event) => {
@@ -15,18 +27,5 @@ document.addEventListener('keydown', (event) => {
   if (!['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(key)) {
     return;
   }
-  const { top, bottom, right, left } = bug.getBoundingClientRect();
-  if (key === 'ArrowUp') {
-    bug.style.top = top - 10 + 'px';
-    bug.style.bottom = bottom + 10 + 'px';
-  } else if (key === 'ArrowDown') {
-    bug.style.top = top + 10 + 'px';
-    bug.style.bottom = bottom - 10 + 'px';
-  } else if (key === 'ArrowLeft') {
-    bug.style.right = right + 10 + 'px';
-    bug.style.left = left - 10 + 'px';
-  } else {
-    bug.style.right = right - 10 + 'px';
-    bug.style.left = left + 10 + 'px';
-  }
+  bugRun(key);
 });
