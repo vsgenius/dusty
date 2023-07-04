f4d7fc4c096b7394042f03e8dca045a41116f2d6
Vladimir Belich <belichvladimir@mail.ru>
2023-07-04 19:21:52 +0300

    fix 3.4 3

--- dom/3.4/3/script.js
+++ dom/3.4/3/script.js
@@ -1,5 +1,14 @@
 const input = document.querySelector('.form-control');
 const blockItems = document.querySelector('.block-items');
+const error = document.querySelector('.error');
+
+blockItems.addEventListener('click', (event) => {
+  const target = event.target;
+  if (!target.classList.contains('btn-close')) return;
+  const item = target.closest('.item');
+  const block = item.closest('.block-items');
+  block.removeChild(item);
+});
 
 function addItem(text) {
   const div = document.createElement('div');
@@ -9,10 +18,6 @@ function addItem(text) {
   divText.textContent = text;
   const btnClose = document.createElement('button');
   btnClose.classList.add('btn-close');
-  btnClose.addEventListener('click', (event) => {
-    const item = event.target.parentElement;
-    item.parentElement.removeChild(item);
-  });
   div.appendChild(divText);
   div.appendChild(btnClose);
   return div;
@@ -20,7 +25,20 @@ function addItem(text) {
 
 input.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
-    blockItems.appendChild(addItem(event.target.value));
+    const value = event.target.value;
+    const texts = document.querySelectorAll('.text');
+    let checkRepeat = false;
+    texts.forEach((text) => {
+      if (value === text.textContent) checkRepeat = true;
+    });
+    if (checkRepeat) {
+      error.classList.remove('hidden');
+      setInterval(() => {
+        error.classList.add('hidden');
+      }, 2000);
+      return;
+    }
+    blockItems.appendChild(addItem(value));
     event.target.value = '';
   }
 });
