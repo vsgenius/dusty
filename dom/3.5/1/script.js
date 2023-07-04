0cbbafc7ee6c2242b049198eb09bed2ca3510c29
Vladimir Belich <belichvladimir@mail.ru>
2023-07-04 19:26:55 +0300

    fix 3.5 1

--- dom/3.5/1/script.js
+++ dom/3.5/1/script.js
@@ -1,12 +1,14 @@
-const predator = document.querySelector('.predator');
+const predators = document.querySelectorAll('.predator');
 
-predator.addEventListener('click', (event) => {
-  const target = event.target;
-  const victim = target.nextElementSibling;
-  if (victim.textContent) {
-    target.textContent += victim.textContent[0];
-    victim.textContent = victim.textContent.slice(1);
-  } else if (victim) {
-    target.parentElement.removeChild(victim);
-  }
+predators.forEach((predator) => {
+  predator.addEventListener('click', (event) => {
+    const target = event.target;
+    const victim = target.nextElementSibling;
+    if (victim.textContent) {
+      target.textContent += victim.textContent[0];
+      victim.textContent = victim.textContent.slice(1);
+    } else if (victim) {
+      target.parentElement.removeChild(victim);
+    }
+  });
 });
