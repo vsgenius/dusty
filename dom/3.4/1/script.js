4dff62bfd444df862b52ab56858548d8dd3172b9
Vladimir Belich <belichvladimir@mail.ru>
2023-07-04 13:09:37 +0300

    fix 3.4 2

--- dom/3.4/1/script.js
+++ dom/3.4/1/script.js
@@ -1,20 +1,33 @@
 const itemsBlock = document.querySelector('.items-block');
 
+function changeStar(count) {
+  const items = document.querySelectorAll('.item-image');
+  items.forEach((element) => {
+    element.src = './svg/black.svg';
+  });
+  for (let i = 0; i < count; i++) {
+    items[i].src = './svg/yellow.svg';
+  }
+}
+
 const handleMouseOver = function (event) {
   const target = event.target;
-  if (target.classList.contains('item-image')) {
-    // console.log(event)
-    if (target.attributes.src.nodeValue === './svg/yellow.svg') {
-      target.attributes.src.nodeValue = './svg/black.svg';
-    } else {
-      target.attributes.src.nodeValue = './svg/yellow.svg';
-    }
+  if (!target.classList.contains('item-image')) {
+    return;
   }
+  changeStar(target.dataset.id);
+};
+
+const handleMouseOut = function () {
+  changeStar(0);
 };
 
 itemsBlock.addEventListener('mouseover', handleMouseOver);
+itemsBlock.addEventListener('mouseleave', handleMouseOut);
+
 const handleClick = function () {
   itemsBlock.removeEventListener('mouseover', handleMouseOver);
   itemsBlock.removeEventListener('click', handleClick);
+  itemsBlock.removeEventListener('mouseleave', handleMouseOut);
 };
 itemsBlock.addEventListener('click', handleClick);
--- dom/3.4/2/script.js
+++ dom/3.4/2/script.js
@@ -1,35 +1,58 @@
 const container = document.querySelector('.container');
 let text;
+let lastTarget;
 
 function addItem(target) {
-  if (target.classList.contains('list-group-item')) {
-    text = target.textContent;
-    target.textContent = '';
-    const input = document.createElement('input');
-    input.classList.add('form-small', 'form-control');
-    input.value = text;
-    target.appendChild(input);
-    const divClear = document.createElement('div');
-    divClear.classList.add('clear');
-    divClear.innerHTML = '&#10006';
-    const divOk = document.createElement('div');
-    divOk.classList.add('ok');
-    divOk.innerHTML = '&#10004';
-    target.appendChild(divClear);
-    target.appendChild(divOk);
-    divClear.addEventListener('click', (event) => {
-      const item = event.target.parentElement;
-      item.textContent = text;
-    });
-    divOk.addEventListener('click', (event) => {
-      const item = event.target.parentElement;
-      item.textContent =
-        event.target.parentElement.querySelector('.form-control').value;
-    });
+  if (!target.classList.contains('list-group-item')) {
+    return;
   }
+  text = target.textContent;
+  target.textContent = '';
+
+  const input = document.createElement('input');
+  input.classList.add('form-small', 'form-control');
+  input.value = text;
+  target.appendChild(input);
+
+  const divClear = document.createElement('div');
+  divClear.classList.add('clear');
+  divClear.innerHTML = '&#10006';
+
+  const divOk = document.createElement('div');
+  divOk.classList.add('ok');
+  divOk.innerHTML = '&#10004';
+  target.appendChild(divClear);
+  target.appendChild(divOk);
+
+  divClear.addEventListener('click', (event) => {
+    const item = event.target.parentElement;
+    item.textContent = text;
+  });
+
+  divOk.addEventListener('click', () => {
+    const item = target.closest('.list-group-item');
+    text = item.querySelector('.form-control').value;
+    item.textContent = item.querySelector('.form-control').value;
+  });
 }
 container.addEventListener('click', (event) => {
-  event.preventDefault;
   const target = event.target;
-  addItem(target);
+
+  if (!lastTarget) {
+    console.log('1');
+    text = target.textContent;
+    addItem(target);
+    lastTarget = target;
+    return;
+  }
+  if (target.querySelector('.form-control')) {
+    return;
+  }
+  if (lastTarget.dataset.id !== target.dataset.id && target.dataset.id) {
+    console.log(text);
+    lastTarget.textContent = text;
+    text = target.textContent;
+    addItem(target);
+    lastTarget = target;
+  }
 });
