3bd57038fa1bee48ea44e026e23a97c600a7deb3
Vladimir Belich <belichvladimir@mail.ru>
2023-07-04 20:43:13 +0300

    fix 3.5 2

--- dom/3.5/2/chat.js
+++ dom/3.5/2/chat.js
@@ -1,58 +1,65 @@
 class Chat {
   constructor(chat) {
     this.chat = chat;
-    this.create();
+    this.listMessages = this.getListMessage();
+    this.chatMessage = document.createElement('div');
+    this.otherMessage = document.createElement('div');
+    this.message = document.createElement('span');
+    this.sendMessage = document.createElement('div');
+    this.inputMessage = document.createElement('input');
+    this.btnMessage = document.createElement('button');
+    this.btnClick = () => {
+      const input = this.inputMessage;
+      if (!input.value) return;
+      this.createMessage('my-message', input.value);
+      this.listMessages[Date.now()] = { key: 'my-message', value: input.value };
+      localStorage.removeItem('chat_3.5.2');
+      localStorage.setItem('chat_3.5.2', JSON.stringify(this.listMessages));
+      input.value = '';
+    };
+    this.init();
   }
   init() {
-    const chatMessage = document.createElement('div');
-    chatMessage.classList.add('chat-message');
-    const otherMessage = document.createElement('div');
-    otherMessage.classList.add('other-message');
-    const message = document.createElement('span');
-    message.classList.add('message');
-    message.textContent = 'привет';
-    otherMessage.appendChild(message);
-    chatMessage.appendChild(otherMessage);
-    this.chat.append(chatMessage);
-    const sendMessage = document.createElement('div');
-    sendMessage.classList.add('send-message');
-    const inputMessage = document.createElement('input');
-    inputMessage.classList.add('input-message');
-    sendMessage.appendChild(inputMessage);
-    const btnMessage = document.createElement('button');
-    btnMessage.classList.add('btn-message');
-    btnMessage.textContent = 'Отправить';
-    sendMessage.appendChild(btnMessage);
-    this.chat.append(sendMessage);
+    this.loadMessage(this.listMessages);
+    this.chatMessage.classList.add('chat-message');
+    this.chat.append(this.chatMessage);
+    this.sendMessage.classList.add('send-message');
+    this.inputMessage.classList.add('input-message');
+    this.sendMessage.appendChild(this.inputMessage);
+    this.btnMessage.classList.add('btn-message');
+    this.btnMessage.textContent = 'Отправить';
+    this.sendMessage.appendChild(this.btnMessage);
+    this.chat.append(this.sendMessage);
+    this.btnMessage.addEventListener('click', this.btnClick);
   }
-  create() {
-    this.init();
-    const btnMessage = document.querySelector('.btn-message');
-    btnMessage.addEventListener('click', (event) => {
-      const input = event.target.parentNode.querySelector('.input-message');
-      if (!input.value) return;
-      this.addMyMessage(input.value);
-      input.value = '';
-    });
+  getListMessage() {
+    // localStorage.removeItem('chat_3.5.2');
+    // localStorage.setItem(
+    //   'chat_3.5.2',
+    //   JSON.stringify({
+    //     1: { key: 'my-message', value: 'Привет' },
+    //     2: { key: 'other-message', value: 'Здарова' },
+    //     3: { key: 'my-message', value: 'как дела?' },
+    //     4: { key: 'other-message', value: 'Отлично' },
+    //     5: { key: 'other-message', value: 'как твои?' },
+    //     6: { key: 'my-message', value: 'Великолепно' },
+    //   })
+    // );
+    const messages = JSON.parse(localStorage.getItem('chat_3.5.2'));
+    return messages;
   }
-  addMyMessage(text) {
-    const chatMessage = this.chat.querySelector('.chat-message');
+  loadMessage(messages) {
+    for (const id in messages) {
+      this.createMessage(messages[id].key, messages[id].value);
+    }
+  }
+  createMessage(classList, text) {
     const div = document.createElement('div');
-    div.classList.add('my-message');
+    div.classList.add(classList);
     const span = document.createElement('span');
     span.classList.add('message');
     span.textContent = text;
     div.appendChild(span);
-    chatMessage.appendChild(div);
-  }
-  addOtherMessage(text) {
-    const chatMessage = this.chat.querySelector('.chat-message');
-    const div = document.createElement('div');
-    div.classList.add('other-message');
-    const span = document.createElement('span');
-    span.classList.add('message');
-    span.textContent = text;
-    div.appendChild(span);
-    chatMessage.appendChild(div);
+    this.chatMessage.appendChild(div);
   }
 }
