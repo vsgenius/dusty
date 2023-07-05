f0460e3fcc68180aa5c04deb2719484fb7a29bd9
Vladimir Belich <belichvladimir@mail.ru>
2023-07-05 17:21:34 +0300

    add 3.6 4

---
+++ dom/3.6/4/script.js
@@ -0,0 +1,57 @@
+const formCheck = document.querySelector('.form-check');
+const selectForm = document.querySelector('.select-form');
+const form = document.querySelector('.form');
+const errorSeat = document.querySelector('.error-seat');
+const errorEat = document.querySelector('.error-eat');
+const container = document.querySelector('.container');
+const formData = {};
+
+function checkSeat(seats) {
+  if (
+    seats &&
+    'abcdef'.includes(seats.slice(-1)) &&
+    seats.slice(0, -1) >= 1 &&
+    seats.slice(0, -1) <= 32
+  ) {
+    return seats;
+  }
+  errorSeat.classList.toggle('hidden');
+  setTimeout(() => {
+    errorSeat.classList.toggle('hidden');
+  }, 1000);
+}
+
+function checkEat(eat) {
+  if (eat && eat !== '...') {
+    return eat;
+  }
+
+  errorEat.classList.toggle('hidden');
+  setTimeout(() => {
+    errorEat.classList.toggle('hidden');
+  }, 1000);
+}
+formCheck.addEventListener('change', () => {
+  selectForm.classList.toggle('hidden');
+});
+
+form.addEventListener('change', (event) => {
+  const target = event.target;
+  if (target.name === 'seat') {
+    formData[target.name] = target.value;
+  } else if (target.name === 'eat') {
+    formData[target.name] = target.checked;
+  } else if (target.name === 'select') {
+    formData[target.name] = target.value;
+  }
+});
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  if (!checkSeat(formData.seat) || (formData.eat && !checkEat(formData.select)))
+    return;
+  container.removeChild(form);
+  const h2 = document.createElement('h2');
+  h2.textContent = 'Регистрация прошла успешно!';
+  container.appendChild(h2);
+});
