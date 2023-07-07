0667163604e794f863a866a7173cee0f8fb5fee8
Vladimir Belich <belichvladimir@mail.ru>
2023-07-07 20:15:57 +0300

    fix 3.5 4

--- dom/3.5/4/script.js
+++ dom/3.5/4/script.js
@@ -1,16 +1,15 @@
-const triangles = document.querySelectorAll('.triangle');
+const container = document.querySelector('.container');
 
-triangles.forEach((triangle) => {
-  triangle.addEventListener('click', (event) => {
-    event.preventDefault();
-    const target = event.target;
-    const element = target.closest('.block').querySelector('.element');
-    if (target.classList.contains('rotate')) {
-      target.classList.remove('rotate');
-      element.style.height = '';
-    } else {
-      target.classList.add('rotate');
-      element.style.height = `${target.offsetHeight * 3}px`;
-    }
-  });
+container.addEventListener('click', (event) => {
+  event.preventDefault();
+  const target = event.target;
+  if (!target.classList.contains('triangle')) return;
+  const element = target.closest('.block').querySelector('.element');
+  if (target.classList.contains('rotate')) {
+    target.classList.remove('rotate');
+    element.style.height = '';
+  } else {
+    target.classList.add('rotate');
+    element.style.height = `${target.offsetHeight * 3}px`;
+  }
 });
