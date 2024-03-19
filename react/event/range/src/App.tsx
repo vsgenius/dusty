189c39c8bf56744ba945fbcf5342e1756cd7a61e
Vladimir Belich <belichvladimir@mail.ru>
2024-03-19 16:14:12 +0300

    fix react event range

--- react/event/range/src/App.tsx
+++ react/event/range/src/App.tsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import { DiscreteSlider } from './components/Slider/Slider';
 import Container from '@material-ui/core/Container';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
 
 import './App.css';
 
@@ -12,21 +17,46 @@ type rgpType = {
 };
 
 function App() {
-  const [rgb, setRgb] = useState<rgpType>({
+  const [rgbFont, setRgbFont] = useState<rgpType>({
     red: 0,
     green: 0,
     blue: 0,
     opacity: 1,
   });
+  const [rgbBack, setRgbBack] = useState<rgpType>({
+    red: 255,
+    green: 255,
+    blue: 255,
+    opacity: 1,
+  });
+  const [currentRadio, setCurrentRadio] = useState<'text' | 'back'>('text');
+  const handleChange = () => {
+    setCurrentRadio(currentRadio === 'text' ? 'back' : 'text');
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <DiscreteSlider rgb={rgb} setRgb={setRgb} />
+        <FormControl>
+          <FormLabel id="demo-customized-radios">Цвет</FormLabel>
+          <RadioGroup
+            defaultValue={currentRadio}
+            aria-labelledby="demo-customized-radios"
+            name="customized-radios"
+            onChange={handleChange}
+          >
+            <FormControlLabel value="text" control={<Radio />} label="Текст" />
+            <FormControlLabel value="back" control={<Radio />} label="Фон" />
+          </RadioGroup>
+        </FormControl>
+        <DiscreteSlider
+          rgb={currentRadio === 'text' ? rgbFont : rgbBack}
+          setRgb={currentRadio === 'text' ? setRgbFont : setRgbBack}
+        />
         <Container
           maxWidth="sm"
           style={{
-            color: `rgb(${rgb.red}, ${rgb.green}, ${rgb.blue})`,
-            opacity: rgb.opacity,
+            color: `rgb(${rgbFont.red}, ${rgbFont.green}, ${rgbFont.blue}, ${rgbFont.opacity})`,
+            backgroundColor: `rgb(${rgbBack.red}, ${rgbBack.green}, ${rgbBack.blue},${rgbBack.opacity})`,
           }}
         >
           <h1>Заголовок</h1>
