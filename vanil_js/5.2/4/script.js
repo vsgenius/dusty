34d7be29e1bffe7a79e92964fa093afd25fe5cff
Vladimir Belich <belichvladimir@mail.ru>
2024-03-23 16:52:51 +0300

    fix vanil_js/5.2/4

--- vanil_js/5.2/4/script.js
+++ vanil_js/5.2/4/script.js
@@ -1,5 +1,7 @@
 const btn = document.querySelector('.btn');
 const blockResult = document.querySelector('.block-result');
+const loading = document.querySelector('.loading');
+const error = document.querySelector('.error');
 
 btn.addEventListener('click', (event) => {
   const target = event.target;
@@ -10,10 +12,17 @@ btn.addEventListener('click', (event) => {
     .closest('.form-group')
     .querySelector('.second-number').value;
   if (!firstNumber && !secondNumber) return;
+  loading.classList.toggle('hidden');
+  error.classList.add('hidden');
   fetch(`http://localhost:3000/?first=${firstNumber}&second=${secondNumber}`)
     .then((response) => response.json())
     .then((result) => {
       blockResult.innerHTML = '';
+      loading.classList.add('hidden');
       blockResult.textContent = result.result;
+    })
+    .catch(() => {
+      loading.classList.add('hidden');
+      error.classList.remove('hidden');
     });
 });
