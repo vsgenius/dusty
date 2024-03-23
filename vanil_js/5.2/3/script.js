fb401385c55c55949cefb12e7d10add06977f938
Vladimir Belich <belichvladimir@mail.ru>
2024-03-23 16:38:40 +0300

    fix vanil_js/5.2/3

--- vanil_js/5.2/3/script.js
+++ vanil_js/5.2/3/script.js
@@ -1,15 +1,17 @@
 const btn = document.querySelector('.btn');
 const facts = document.querySelector('.facts');
 const loading = document.querySelector('.loading');
+const error = document.querySelector('.error');
 
 btn.addEventListener('click', (event) => {
   const target = event.target;
   target.setAttribute('disabled', '');
+  error.classList.add('hidden');
   loading.classList.remove('hidden');
   facts.textContent = '';
   const number = document.querySelector('.number').value;
   if (!number) return;
-  fetch(`https://dog-api.kinduff.com/api/facts?number=${number}`)
+  fetch(`https://dog-api.kinduff.сom/api/facts?number=${number}`)
     .then((response) => response.json())
     .then((result) => {
       loading.classList.add('hidden');
@@ -19,5 +21,10 @@ btn.addEventListener('click', (event) => {
         p.textContent = element;
         facts.appendChild(p);
       });
+    })
+    .catch(() => {
+      loading.classList.add('hidden');
+      error.classList.remove('hidden');
+      target.removeAttribute('disabled');
     });
 });
